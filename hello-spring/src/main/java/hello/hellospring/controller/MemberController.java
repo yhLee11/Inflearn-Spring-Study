e94668286a2728c66d61837753997c433085b984
yhLee11 <mrncd1@gmail.com>
2022-01-24 00:38:29 +0900

    16 Get,Post Mapping

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -10,17 +13,21 @@ public class MemberController {
 
     //생성자 주입
     @Autowired//스프링이 스프링 컨테이너에 있는 memberService와 연결시켜 주는 역할
-    public MemberController(MemberService memberService){//생성자로 한번 등록함
-        this.memberService=memberService;
+    public MemberController(MemberService memberService) {//생성자로 한번 등록함
+        this.memberService = memberService;
     }
 
-    //필드 주입
-//    @Autowired private MemberService memberService;
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
-    //setter 주입
-//    @Autowired
-//    public void setMemberService(MemberService memberService){
-//        this.memberService = memberService;
-//    }
-
+    @PostMapping("/members/new")
+    public  String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println("member="+member.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
