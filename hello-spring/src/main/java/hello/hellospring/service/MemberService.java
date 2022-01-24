09acb0a94bdae3f3f5a1528be7758fa1a4634154
yhLee11 <mrncd1@gmail.com>
2022-01-24 13:48:03 +0900

    24 AOP

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -17,9 +17,19 @@ public class MemberService {
 
     //회원가입
     public Long join(Member member){
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = "+timeMs+"ms");
+        }
+
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -30,7 +40,15 @@ public class MemberService {
     }
 
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try{
+            return memberRepository.findAll();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers "+timeMs+"ms");
+        }
+
     }
 
     public Optional<Member> findOne(Long memberId){
