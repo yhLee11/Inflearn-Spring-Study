48dde45094a5ee1b49efef772c6364b20809dae6
yhLee11 <mrncd1@gmail.com>
2022-01-24 11:51:31 +0900

    19 jdbc repository

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,23 @@
 package hello.hellospring;
 
-import hello.hellospring.controller.MemberController;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource=dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());//스프링 빈에 등록되어 있는 memberRepository를 MemberService에 넣어준다.
@@ -17,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {//인터페이스는 new 안됨
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
