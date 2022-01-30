1b9514b6a78638c6f4e0a8e94d5e575ef9f10e65
yhLee11 <mrncd1@gmail.com>
2022-01-30 17:01:36 +0900

    스프링빈 조회, 메타정보

---
+++ core/src/test/java/hello/core/beanfind/AnnotationConfigApplicationBasicFindTest.java
@@ -0,0 +1,31 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnnotationConfigApplicationBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으만 조회")
+    void findBeanByType() {
+//        MemberService memberService = ac.
+    }
+
+}
