e48eb4794338b39e4686d29faedc4b8e9dcdec59
yhLee11 <mrncd1@gmail.com>
2022-04-07 16:42:55 +0900

    Entity class

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+}
