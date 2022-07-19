d70ed92b029d5873fe5b97f538de35867ba9c33e
yhLee11 <mrncd1@gmail.com>
2022-07-19 22:21:09 +0900

    Model 추가 - v3

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRespository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+    private MemberRespository memberRespository = MemberRespository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paraMap) {
+        List<Member> members = memberRespository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+        return mv;
+    }
+}
